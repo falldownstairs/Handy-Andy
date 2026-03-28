71c6b9487635fd02ab31037d6f7c2bd13d9695fe
Toricane <prjwl028@gmail.com>
2026-03-27 19:26:11 -0700

    add back the comment referencing pin connections

--- arduinoConfig/arduinoConfig.ino
+++ arduinoConfig/arduinoConfig.ino
@@ -1,7 +1,15 @@
 /*
 Initial testing and calibration of servo motors connected to the
 Adeept HAT, which is wired to the Arduino Mega 2560 R3 board.
-*/
+
+Connections from HAT P1 Header to Arduino:
+Pin 1 (3.3V) -> 3.3V
+Pin 3 (SDA1) -> 20 (SDA)
+Pin 5 (SCL1) -> 21 (SCL)
+Pin 6 (GND)  -> GND
+
+Referenced from Adeept_RaspClaws-V2.0-20251030/Circuit-Schematic-Adeept_Robot-HAT.pdf
+*/ 
 
 #include <Arduino.h>
 #include "Kinematics.h"
